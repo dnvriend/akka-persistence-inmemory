3de10e512b8d81bd046ed8e2e5482ad2f6234a3d
Dennis Vriend <dennis.vriend@trivento.nl>
2015-05-25 17:32:43 +0200

    #1 and #2 issue test

--- src/test/scala/akka/persistence/inmemory/journal/JournalTest.scala
+++ src/test/scala/akka/persistence/inmemory/journal/JournalTest.scala
@@ -1,9 +1,10 @@
 package akka.persistence.inmemory.journal
 
 import akka.actor.{PoisonPill, Props}
+import akka.event.LoggingReceive
+import akka.pattern.ask
 import akka.persistence.PersistentActor
 import akka.persistence.inmemory.TestSpec
-import akka.pattern.ask
 
 class JournalTest extends TestSpec {
 
@@ -15,38 +16,57 @@ class JournalTest extends TestSpec {
     }
   }
 
-  class Counter extends PersistentActor {
+  class Counter(id: Int) extends PersistentActor {
     var state = CounterState(0)
 
-    override val persistenceId: String = "counter"
+    override val persistenceId: String = "c-" + id
 
     def updateState(event: String): Unit = {
       state = state.update(event)
     }
 
-    override def receiveCommand: Receive = {
+    override def receiveCommand: Receive = LoggingReceive {
       case "state" =>
         sender() ! state.counter
 
+      case "deleteAll" =>
+        deleteMessages(Long.MaxValue, permanent = true)
+
       case event: String =>
-        persist(event) { event =>
+        persist(event) { (event: String) =>
           updateState(event)
         }
     }
 
-    override def receiveRecover: Receive = {
+    override def receiveRecover: Receive = LoggingReceive {
       case event: String => state = state.update(event)
     }
   }
 
 
   "Counter" should "persist state" in {
-    var counter = system.actorOf(Props(new Counter))
+    var counter = system.actorOf(Props(new Counter(1)))
     counter ! "add"
     counter ! "add"
     (counter ? "state").futureValue shouldBe 2
-    counter ! PoisonPill
-    counter = system.actorOf(Props(new Counter))
+    cleanup(counter)
+    counter = system.actorOf(Props(new Counter(1)))
     (counter ? "state").futureValue shouldBe 2
+    cleanup(counter)
+  }
+
+  it should "#1 and #2: delete all journal entries gives 'empty.max' on collection" in {
+    var counter = system.actorOf(Props(new Counter(2)))
+    counter ! "add"
+    counter ! "add"
+    (counter ? "state").futureValue shouldBe 2
+    cleanup(counter)
+    counter = system.actorOf(Props(new Counter(2)))
+    (counter ? "state").futureValue shouldBe 2
+    counter ! "deleteAll"
+    cleanup(counter)
+    counter = system.actorOf(Props(new Counter(2)))
+    (counter ? "state").futureValue shouldBe 0
+    cleanup(counter)
   }
 }
